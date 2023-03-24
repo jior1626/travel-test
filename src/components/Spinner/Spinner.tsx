1820f1ab2b12375a37ec8de1247acb83f550da9d
Jordan Orozco <joaorozc@bancolombia.com.co>
2023-03-24 17:00:34 -0500

    Nuevos componentes para estructura base

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+import "./Spinner.css";
+
+const Spinner: React.FC<any> = ({}) => {
+    return (
+        <></>
+    )
+}
+
+export default Spinner;
---
+++ src/components/layout/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+import "./Navbar.css";
+
+const Navbar: React.FC<any> = ({}) => {
+    return (
+        <></>
+    )
+}
+
+export default Navbar;
