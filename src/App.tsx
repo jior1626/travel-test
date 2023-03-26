bbb79841c54286d54595dc76d3e74224232b69a0
Jordan Orozco <joaorozc@bancolombia.com.co>
2023-03-26 01:45:18 -0500

    New changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider  } from 'react-router-dom';
+import Login from './pages/Auth/Login';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
+      <Route path="/" element={<Login />} />
       {/* <Route path="/" element={} />
       <Route path="/home" element={<Middleware Element={<Home />}/>} />
       <Route path="/users" element={<Middleware Element={<Users />}/>} /> */}
--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -3,9 +3,15 @@ import React from "react";
 
 import "./Spinner.css";
 
+import { MDBSpinner } from 'mdb-react-ui-kit';
+
 const Spinner: React.FC<any> = ({}) => {
     return (
-        <></>
+        <div className="loading">
+			<MDBSpinner color='success' >
+				<span className='visually-hidden'>Loading...</span>
+			</MDBSpinner>
+		</div>
     )
 }
 
