26920fa73bfdbf0fa3b961844a6d84f4b3708cb6
jior1626 <jordarturo1998@gmail.com>
2023-03-26 02:55:26 -0500

    Add booking dto

---
+++ src/models/booking.ts
@@ -0,0 +1,11 @@
+import { UserDto } from "./user";
+import { Bedroom } from "./bedroom";
+
+export interface Booking {
+    id?: number;
+    user: UserDto;
+    bedroom?: Bedroom;
+    startDate: string;
+    finalDate: string;
+    state: string
+}
